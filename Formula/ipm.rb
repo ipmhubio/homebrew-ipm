a580edaa75a91c64fc059d11a58c4af7d1a1be4f
Bas <61052187+BasBerk@users.noreply.github.com>
2025-02-06 08:22:16 +0100

    Update ipm.rb

--- Formula/ipm.rb
+++ Formula/ipm.rb
@@ -3,10 +3,10 @@ class Ipm < Formula
     homepage "https://ipmhub.io"
   
     if Hardware::CPU.arm?
-      url "https://github.com/ipmhubio/ipm/releases/download/0.6.0/ipm-osx-arm64-full.zip"
+      url "https://github.com/ipmhubio/ipm/releases/download/0.7.0/ipm-osx-arm64-full.zip"
       sha256 "5283b28163b8aad48138c93a1ab10b583285392478ac948fd1ee503cc14bfce9"
     else
-      url "https://github.com/ipmhubio/ipm/releases/download/0.6.0/ipm-osx-x64-full.zip"
+      url "https://github.com/ipmhubio/ipm/releases/download/0.7.0/ipm-osx-x64-full.zip"
       sha256 "19e30ad2a9124595b7dd0c285d175cc09487b461712440c54ba6be8282cbf97e"
     end
   
